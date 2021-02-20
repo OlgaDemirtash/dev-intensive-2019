c71683a677b494593d85efeb449ab56ac35b42d3
Olga <olga.demirtash@gmail.com>
2021-02-20 16:56:05 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -17,6 +17,7 @@ import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 
 
@@ -137,33 +138,3 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 }
 
-// Add these extension functions to an empty kotlin file
-fun Activity.getRootView(): View {
-    return findViewById<View>(android.R.id.content)
-}
-
-fun Context.convertDpToPx(dp: Float): Float {
-    return TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP,
-        dp,
-        this.resources.displayMetrics
-    )
-}
-
-fun Activity.isKeyboardOpen(): Boolean {
-    val visibleBounds = Rect()
-    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
-    val heightDiff = getRootView().height - visibleBounds.height()
-    val marginOfError = Math.round(this.convertDpToPx(50F))
-    return heightDiff > marginOfError
-}
-
-fun Activity.isKeyboardClosed(): Boolean {
-    return !this.isKeyboardOpen()
-}
-
-fun Activity.hideKeyboard() {
-    val view: View = currentFocus ?: View(this)
-    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-}
